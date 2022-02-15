c86099086552f8edf099309618c5d2fefc5dd987
Yadav Ashutosh Satish <66318596+thedevashu@users.noreply.github.com>
2022-02-15 09:10:48 +0530

    first commit

---
+++ HelloWorldApp/HelloWorldApp/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldApp
+{
+    class Person
+    {
+        //class member
+        private string _firstname;
+
+        //geter and seter
+        public string FirstName
+        {
+            get
+            {
+                return _firstname;
+            }
+
+            set
+            {
+                _firstname = value;
+            }
+        }
+
+    }
+}
---
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<10;i++)
+            Console.WriteLine("Hello World!");
+            Person p = new Person();
+            p.FirstName = "oyoy";
+            Console.WriteLine(p.FirstName);
+        }
+    }
+}
